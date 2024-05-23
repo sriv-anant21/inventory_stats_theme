60fcb612a8a761626cbce0df33aa16a39bfc0269
sriv-anant21 <sriv.anant21@gmail.com>
2024-05-24 01:20:09 +0530

    fix.

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { connectDb } = require("./src/config/db.js");
+const PORT = 5454;
+const authRouters = require("./src/routes/auth.route.js");
+const userRouters = require("./src/routes/user.route.js");
+const preferenceRouters = require("./src/routes/preference.route.js");
+
+const app = express();
+
+app.use(express.json()); //middleware
+app.use(cors()); // to allow all URLs from frontend
+
+app.listen(PORT, async () => {
+  await connectDb();
+  console.log("Backend is listening on PORT : ", PORT);
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).send({ message: "Welcome- Backend is listening on PORT : ", PORT, status: true });
+});
+
+app.use("/auth", authRouters); // redirect all /auth to authRouters
+app.use("/user", userRouters); // redirect all /user to userRouters
+app.use("/preference",preferenceRouters); // redirect all /preference to preferenceRouters
+
+module.exports = app;
--- server/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require(".");
-const { connectDb } = require("./config/db");
-const PORT = 5454;
-
-app.listen(PORT, async () => {
-  await connectDb();
-  console.log("Backend is listening on PORT : ", PORT);
-});
