5368731babd5db5ab0fe8b032313a2c786781489
sriv-anant21 <36619194+sriv-anant21@users.noreply.github.com>
2024-05-24 16:29:08 +0530

    Update store.tsx

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import {thunk} from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import inventoryReducer from './inventoryStates/slice';
 import themeReducer from './themeStates/slice';
